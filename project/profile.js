07fffe4d84d43010fa5b9cc8610e27645d7cc240
unknown <abdelmajied.abusuliman@gmail.com>
2022-11-19 00:59:03 +0300

    Project 3

--- project/profile.js
+++ project/profile.js
@@ -7,11 +7,11 @@ let currentUser = JSON.parse(localStorage.getItem("current-user"));
 
 
 
-// currentUser["coachPic"] = "https://media-exp1.licdn.com/dms/image/C4E03AQH_0kbkqu-0TA/profile-displayphoto-shrink_800_800/0/1633597935445?e=1674086400&v=beta&t=oewLyHFc-pdkDwEvD5upt7LzgVMPcA1h-J7oTt0Dw1M"
-// localStorage.setItem("current-user", JSON.stringify(currentUser))
+currentUser["coachPic"] = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__480.png"
+localStorage.setItem("current-user", JSON.stringify(currentUser))
 
-// arrayOfKeysAndValues[1]["coachPic"] = "https://media-exp1.licdn.com/dms/image/C4E03AQH_0kbkqu-0TA/profile-displayphoto-shrink_800_800/0/1633597935445?e=1674086400&v=beta&t=oewLyHFc-pdkDwEvD5upt7LzgVMPcA1h-J7oTt0Dw1M"
-// localStorage.setItem("userInfo", JSON.stringify(arrayOfKeysAndValues))
+arrayOfKeysAndValues[2]["coachPic"] = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__480.png"
+localStorage.setItem("userInfo", JSON.stringify(arrayOfKeysAndValues))
 
 
 
