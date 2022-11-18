1d1a091501ddf688a8cd35695642979234b7ed51
unknown <abdelmajied.abusuliman@gmail.com>
2022-11-19 00:42:08 +0300

    Project 3

--- project/profile.js
+++ project/profile.js
@@ -7,10 +7,10 @@ let currentUser = JSON.parse(localStorage.getItem("current-user"));
 
 
 
-currentUser["coachPic"] = "https://media-exp1.licdn.com/dms/image/C4E03AQFO1f75-n-0Xg/profile-displayphoto-shrink_800_800/0/1656781140615?e=1674086400&v=beta&t=oeJn8JyhW-MtKCxoq6qFg9VPgS5fh1ts2Wi6l_YWGxU"
+currentUser["coachPic"] = "https://media-exp1.licdn.com/dms/image/C4E03AQH_0kbkqu-0TA/profile-displayphoto-shrink_800_800/0/1633597935445?e=1674086400&v=beta&t=oewLyHFc-pdkDwEvD5upt7LzgVMPcA1h-J7oTt0Dw1M"
 localStorage.setItem("current-user", JSON.stringify(currentUser))
 
-arrayOfKeysAndValues[0]["coachPic"] = "https://media-exp1.licdn.com/dms/image/C4E03AQFO1f75-n-0Xg/profile-displayphoto-shrink_800_800/0/1656781140615?e=1674086400&v=beta&t=oeJn8JyhW-MtKCxoq6qFg9VPgS5fh1ts2Wi6l_YWGxU"
+arrayOfKeysAndValues[0]["coachPic"] = "https://media-exp1.licdn.com/dms/image/C4E03AQH_0kbkqu-0TA/profile-displayphoto-shrink_800_800/0/1633597935445?e=1674086400&v=beta&t=oewLyHFc-pdkDwEvD5upt7LzgVMPcA1h-J7oTt0Dw1M"
 localStorage.setItem("userInfo", JSON.stringify(arrayOfKeysAndValues))
 
 
