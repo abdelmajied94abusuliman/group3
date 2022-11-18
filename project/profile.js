8090c778685ab1412a6ff1f50c80082c8a6e6e27
unknown <abdelmajied.abusuliman@gmail.com>
2022-11-19 00:43:01 +0300

    Project 3

--- project/profile.js
+++ project/profile.js
@@ -10,7 +10,7 @@ let currentUser = JSON.parse(localStorage.getItem("current-user"));
 currentUser["coachPic"] = "https://media-exp1.licdn.com/dms/image/C4E03AQH_0kbkqu-0TA/profile-displayphoto-shrink_800_800/0/1633597935445?e=1674086400&v=beta&t=oewLyHFc-pdkDwEvD5upt7LzgVMPcA1h-J7oTt0Dw1M"
 localStorage.setItem("current-user", JSON.stringify(currentUser))
 
-arrayOfKeysAndValues[0]["coachPic"] = "https://media-exp1.licdn.com/dms/image/C4E03AQH_0kbkqu-0TA/profile-displayphoto-shrink_800_800/0/1633597935445?e=1674086400&v=beta&t=oewLyHFc-pdkDwEvD5upt7LzgVMPcA1h-J7oTt0Dw1M"
+arrayOfKeysAndValues[1]["coachPic"] = "https://media-exp1.licdn.com/dms/image/C4E03AQH_0kbkqu-0TA/profile-displayphoto-shrink_800_800/0/1633597935445?e=1674086400&v=beta&t=oewLyHFc-pdkDwEvD5upt7LzgVMPcA1h-J7oTt0Dw1M"
 localStorage.setItem("userInfo", JSON.stringify(arrayOfKeysAndValues))
 
 
